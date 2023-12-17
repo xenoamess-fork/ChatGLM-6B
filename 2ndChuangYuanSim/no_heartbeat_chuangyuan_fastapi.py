33b385fb34e7c6b11db0ce301d7a2f40b7ef43e2
XenoAmess <xenoamess@gmail.com>
2023-12-17 23:45:17 +0800

    添加fastapi模拟创院

---
+++ 2ndChuangYuanSim/no_heartbeat_chuangyuan_fastapi.py
@@ -0,0 +1,107 @@
+from fastapi import FastAPI, Request
+from fastapi.responses import StreamingResponse
+from transformers import AutoTokenizer, AutoModel
+import uvicorn, json, datetime
+import torch
+
+DEVICE = "cuda"
+DEVICE_ID = "0"
+CUDA_DEVICE = f"{DEVICE}:{DEVICE_ID}" if DEVICE_ID else DEVICE
+
+
+def torch_gc():
+    if torch.cuda.is_available():
+        with torch.cuda.device(CUDA_DEVICE):
+            torch.cuda.empty_cache()
+            torch.cuda.ipc_collect()
+
+
+app = FastAPI()
+
+def predict(
+        tokenizer,
+        prompt,
+        history,
+        max_length,
+        top_p,
+        temperature
+):
+    global fullLastResponse
+    fullLastResponse = ""
+    for response, history in model.stream_chat(
+            tokenizer,
+            prompt,
+            history,
+            max_length=max_length,
+            top_p=top_p,
+            temperature=temperature
+    ):
+        fullLastResponseLength = len(fullLastResponse)
+        if len(response) > fullLastResponseLength:
+            currentResponse = response[fullLastResponseLength:]
+        else:
+            currentResponse = ""
+        print(currentResponse)
+        yield currentResponse + "\n"
+        fullLastResponse = response
+
+@app.post("/fastapi")
+async def fastapi(request: Request):
+    global model, tokenizer
+    json_post_raw = await request.json()
+    json_post = json.dumps(json_post_raw)
+    json_post_list = json.loads(json_post)
+    prompt = json_post_list.get('prompt')
+    history = json_post_list.get('history')
+    max_length = json_post_list.get('max_length')
+    top_p = json_post_list.get('top_p')
+    temperature = json_post_list.get('temperature')
+    # torch_gc()
+    return StreamingResponse(
+        predict(
+               tokenizer,
+               prompt,
+               history=history,
+               max_length=max_length if max_length else 2048,
+               top_p=top_p if top_p else 0.7,
+               temperature=temperature if temperature else 0.95
+        ),
+        media_type="octet/stream"
+    )
+
+@app.post("/")
+async def create_item(request: Request):
+    global model, tokenizer
+    json_post_raw = await request.json()
+    json_post = json.dumps(json_post_raw)
+    json_post_list = json.loads(json_post)
+    prompt = json_post_list.get('prompt')
+    history = json_post_list.get('history')
+    max_length = json_post_list.get('max_length')
+    top_p = json_post_list.get('top_p')
+    temperature = json_post_list.get('temperature')
+    response, history = model.chat(tokenizer,
+                                   prompt,
+                                   history=history,
+                                   max_length=max_length if max_length else 2048,
+                                   top_p=top_p if top_p else 0.7,
+                                   temperature=temperature if temperature else 0.95)
+    now = datetime.datetime.now()
+    time = now.strftime("%Y-%m-%d %H:%M:%S")
+    answer = {
+        "response": response,
+        "history": history,
+        "status": 200,
+        "time": time
+    }
+    log = "[" + time + "] " + '", prompt:"' + prompt + '", response:"' + repr(response) + '"'
+    print(log)
+    torch_gc()
+    return answer
+
+
+if __name__ == '__main__':
+    tokenizer = AutoTokenizer.from_pretrained("THUDM/chatglm-6b", trust_remote_code=True)
+    model = AutoModel.from_pretrained("THUDM/chatglm-6b", trust_remote_code=True).half().cuda()
+    model.eval()
+    uvicorn.run(app, host='0.0.0.0', port=8000, workers=1)
